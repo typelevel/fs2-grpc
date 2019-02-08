1d82bda50f5bc7c3295429895404b040210796ec
Alex Henning Johannessen <ahjohannessen@gmail.com>
2019-02-08 14:50:50 +0000

    codegen: redo metadata
     - change code generator to parameterize metadata instead
       of hardcoding to `io.grpc.Metadata`.
    
     - add `client` and `service` methods to service companion
       that take a function from `A => Metadata` and
       `Metadata => Either[String, A]`, respectively.
    
     - add option to specify suffix for generated code.
    
     - change `client` method to take function
       `c: CallOptions => CallOptions` in order
       to be able to use `withDeadline` for each
       call. Current approach requires newing up
       a new client for each call as a deadline is
       absolute.

--- sbt-java-gen/src/main/scala/Fs2GrpcServicePrinter.scala
+++ sbt-java-gen/src/main/scala/Fs2GrpcServicePrinter.scala
@@ -4,24 +4,25 @@ import com.google.protobuf.Descriptors.{MethodDescriptor, ServiceDescriptor}
 import scalapb.compiler.FunctionalPrinter.PrinterEndo
 import scalapb.compiler.{DescriptorImplicits, FunctionalPrinter, StreamType}
 
-class Fs2GrpcServicePrinter(service: ServiceDescriptor, di: DescriptorImplicits){
+class Fs2GrpcServicePrinter(service: ServiceDescriptor, serviceSuffix: String, di: DescriptorImplicits){
   import di._
   import Fs2GrpcServicePrinter.constants._
 
   private[this] val serviceName: String    = service.name
+  private[this] val serviceNameFs2: String = s"$serviceName$serviceSuffix"
   private[this] val servicePkgName: String = service.getFile.scalaPackageName
 
   private[this] def serviceMethodSignature(method: MethodDescriptor) = {
 
     val scalaInType   = method.inputType.scalaType
     val scalaOutType  = method.outputType.scalaType
-    val clientHeaders = s"clientHeaders: $Metadata"
+    val ctx           = s"ctx: $Ctx"
 
     s"def ${method.name}" + (method.streamType match {
-      case StreamType.Unary           => s"(request: $scalaInType, $clientHeaders): F[$scalaOutType]"
-      case StreamType.ClientStreaming => s"(request: $Stream[F, $scalaInType], $clientHeaders): F[$scalaOutType]"
-      case StreamType.ServerStreaming => s"(request: $scalaInType, $clientHeaders): $Stream[F, $scalaOutType]"
-      case StreamType.Bidirectional   => s"(request: $Stream[F, $scalaInType], $clientHeaders): $Stream[F, $scalaOutType]"
+      case StreamType.Unary           => s"(request: $scalaInType, $ctx): F[$scalaOutType]"
+      case StreamType.ClientStreaming => s"(request: $Stream[F, $scalaInType], $ctx): F[$scalaOutType]"
+      case StreamType.ServerStreaming => s"(request: $scalaInType, $ctx): $Stream[F, $scalaOutType]"
+      case StreamType.Bidirectional   => s"(request: $Stream[F, $scalaInType], $ctx): $Stream[F, $scalaOutType]"
     })
   }
 
@@ -36,7 +37,7 @@ class Fs2GrpcServicePrinter(service: ServiceDescriptor, di: DescriptorImplicits)
 
   private[this] def createClientCall(method: MethodDescriptor) = {
     val basicClientCall =
-      s"$Fs2ClientCall[F](channel, _root_.$servicePkgName.${serviceName}Grpc.${method.descriptorName}, callOptions)"
+      s"$Fs2ClientCall[F](channel, _root_.$servicePkgName.${serviceName}Grpc.${method.descriptorName}, c($CallOptions.DEFAULT))"
     if (method.isServerStreaming)
       s"$Stream.eval($basicClientCall)"
     else
@@ -47,17 +48,23 @@ class Fs2GrpcServicePrinter(service: ServiceDescriptor, di: DescriptorImplicits)
     p.add(serviceMethodSignature(method) + " = {")
       .indent
       .add(
-        s"${createClientCall(method)}.flatMap(_.${handleMethod(method)}(request, clientHeaders))"
+        s"${createClientCall(method)}.flatMap(_.${handleMethod(method)}(request, f(ctx)))"
       )
       .outdent
       .add("}")
   }
 
-  // TODO: update this
   private[this] def serviceBindingImplementation(method: MethodDescriptor): PrinterEndo = { p =>
-    p.add(
-      s".addMethod(_root_.$servicePkgName.${serviceName}Grpc.${method.descriptorName}, $Fs2ServerCallHandler[F].${handleMethod(
-        method)}(serviceImpl.${method.name}))")
+
+    val inType      = method.inputType.scalaType
+    val outType     = method.outputType.scalaType
+    val descriptor  = s"_root_.$servicePkgName.${serviceName}Grpc.${method.descriptorName}"
+    val handler     = s"$Fs2ServerCallHandler[F].${handleMethod(method)}[$inType, $outType]"
+
+    val serviceCall = s"serviceImpl.${method.name}"
+    val eval        = if(method.isServerStreaming) s"$Stream.eval(g(m))" else "g(m)"
+
+    p.add(s".addMethod($descriptor, $handler((r, m) => $eval.flatMap($serviceCall(r, _))))")
   }
 
   private[this] def serviceMethods: PrinterEndo = _.seq(service.methods.map(serviceMethodSignature))
@@ -73,14 +80,30 @@ class Fs2GrpcServicePrinter(service: ServiceDescriptor, di: DescriptorImplicits)
       .outdent
 
   private[this] def serviceTrait: PrinterEndo =
-    _.add(s"trait ${serviceName}Fs2Grpc[F[_]] {").indent.call(serviceMethods).outdent.add("}")
+    _.add(s"trait $serviceNameFs2[F[_], $Ctx] {").
+      indent.
+      call(serviceMethods).
+      outdent.
+      add("}")
 
   private[this] def serviceObject: PrinterEndo =
-    _.add(s"object ${serviceName}Fs2Grpc {").indent.call(serviceClient).call(serviceBinding).outdent.add("}")
+    _.add(s"object $serviceNameFs2 {").
+      indent.
+      newline.
+      call(serviceClientMeta).
+      newline.
+      call(serviceClient).
+      newline.
+      call(serviceBindingMeta).
+      newline.
+      call(serviceBinding).
+      outdent.
+      add("}")
 
   private[this] def serviceClient: PrinterEndo = {
     _.add(
-      s"def stub[F[_]: $ConcurrentEffect](channel: $Channel, callOptions: $CallOptions = $CallOptions.DEFAULT): ${serviceName}Fs2Grpc[F] = new ${serviceName}Fs2Grpc[F] {").indent
+      s"def client[F[_]: $ConcurrentEffect, $Ctx](channel: $Channel, f: $Ctx => $Metadata, c: $CallOptions => $CallOptions = identity): $serviceNameFs2[F, $Ctx] = new $serviceNameFs2[F, $Ctx] {")
+      .indent
       .call(serviceMethodImplementations)
       .outdent
       .add("}")
@@ -88,17 +111,43 @@ class Fs2GrpcServicePrinter(service: ServiceDescriptor, di: DescriptorImplicits)
 
   private[this] def serviceBinding: PrinterEndo = {
     _.add(
-      s"def bindService[F[_]: $ConcurrentEffect](serviceImpl: ${service.name}Fs2Grpc[F]): $ServerServiceDefinition = {").indent
+      s"def service[F[_]: $ConcurrentEffect, $Ctx](serviceImpl: $serviceNameFs2[F, $Ctx], f: $Metadata => Either[$Error, $Ctx]): $ServerServiceDefinition = {")
+      .indent
+      .newline
+      .add(s"val g: $Metadata ⇒ F[$Ctx] = f(_).leftMap[Throwable]($FailedPrecondition.withDescription(_).asRuntimeException()).raiseOrPure[F]")
+      .newline
       .add(s"$ServerServiceDefinition")
       .call(serviceBindingImplementations)
       .outdent
       .add("}")
   }
 
+  /// For those that prefer io.grpc.Metadata instead of parameterized context
+
+  private[this] def serviceClientMeta: PrinterEndo =
+    _.add(
+      s"def stub[F[_]: $ConcurrentEffect](channel: $Channel, callOptions: $CallOptions = $CallOptions.DEFAULT): $serviceNameFs2[F, $Metadata] = {")
+      .indent
+      .add(s"client[F, $Metadata](channel, identity, _ => callOptions)")
+      .outdent
+      .add("}")
+
+  private[this] def serviceBindingMeta: PrinterEndo = {
+    _.add(
+      s"def bindService[F[_]: $ConcurrentEffect](serviceImpl: $serviceNameFs2[F, $Metadata]): $ServerServiceDefinition = {")
+      .indent
+      .add(s"service[F, $Metadata](serviceImpl, _.asRight[$Error])")
+      .outdent
+      .add("}")
+  }
+
+  ///
+
   def printService(printer: FunctionalPrinter): FunctionalPrinter = {
     printer
       .add(s"package $servicePkgName", "", "import _root_.cats.implicits._", "")
       .call(serviceTrait)
+      .newline
       .call(serviceObject)
   }
 }
@@ -114,14 +163,20 @@ object Fs2GrpcServicePrinter {
 
     ///
 
+    val Error                   = "String"
+    val Ctx                     = "A"
+
     val ConcurrentEffect        = s"$effPkg.ConcurrentEffect"
     val Stream                  = s"$fs2Pkg.Stream"
+
     val Fs2ServerCallHandler    = s"$jrtPkg.server.Fs2ServerCallHandler"
     val Fs2ClientCall           = s"$jrtPkg.client.Fs2ClientCall"
+
     val ServerServiceDefinition = s"$grpcPkg.ServerServiceDefinition"
     val CallOptions             = s"$grpcPkg.CallOptions"
     val Channel                 = s"$grpcPkg.Channel"
     val Metadata                = s"$grpcPkg.Metadata"
+    val FailedPrecondition      = s"$grpcPkg.Status.FAILED_PRECONDITION"
 
   }
 

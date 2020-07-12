e5eeb4d14e53d5111532b75864dec1bc832283b5
Scala Steward <me@scala-steward.org>
2020-07-12 20:23:08 +0200

    Update cats-effect, cats-effect-laws to 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val grpc = scalapb.compiler.Version.grpcJavaVersion
     val scalaPb = scalapb.compiler.Version.scalapbVersion
     val fs2 = "2.4.2"
-    val catsEffect = "2.1.3"
+    val catsEffect = "2.1.4"
     val minitest = "2.8.2"
 
     val kindProjector = "0.10.3"

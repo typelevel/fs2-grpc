64c1fd53460d5f24dac9969b51fd48b9e14f69ba
Scala Steward <me@scala-steward.org>
2020-12-22 02:31:12 +0100

    Update fs2-core to 3.0.0-M7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val grpc = scalapb.compiler.Version.grpcJavaVersion
     val scalaPb = scalapb.compiler.Version.scalapbVersion
 
-    val fs2 = "3.0.0-M6"
+    val fs2 = "3.0.0-M7"
     val catsEffect = "3.0.0-M4"
     val ceMunit = "0.11.0"
 

be999df4a811f27c55e33c88d4c2f390a6ba8f81
Scala Steward <me@scala-steward.org>
2021-02-17 20:47:21 +0100

    Update munit-cats-effect-3 to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
     val fs2 = "3.0-229-d6e036f"
     val catsEffect = "3.0.0-RC1"
-    val ceMunit = "0.13.0"
+    val ceMunit = "0.13.1"
 
     val kindProjector = "0.10.3"
     val sbtProtoc = "1.0.1"

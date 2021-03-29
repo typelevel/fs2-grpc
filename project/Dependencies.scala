1245525dae4b44fac2c6ef65364d99f34ed12c13
Scala Steward <me@scala-steward.org>
2021-03-29 11:54:53 +0200

    Update cats-effect, cats-effect-testkit to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val scalaPb = scalapb.compiler.Version.scalapbVersion
 
     val fs2 = "3.0.0-M9"
-    val catsEffect = "3.0.0-RC3"
+    val catsEffect = "3.0.0"
     val ceMunit = "0.13.1"
 
     val kindProjector = "0.10.3"

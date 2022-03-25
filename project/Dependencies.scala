d74a1b48c9746eec3471f9edd2ee1d712d495598
Scala Steward <me@scala-steward.org>
2022-03-25 05:07:10 +0100

    Update cats-effect, cats-effect-testkit to 3.3.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val scalaPb = scalapb.compiler.Version.scalapbVersion
 
     val fs2 = "3.2.5"
-    val catsEffect = "3.3.8"
+    val catsEffect = "3.3.9"
     val ceMunit = "1.0.7"
 
     val sbtProtoc = "1.0.6"

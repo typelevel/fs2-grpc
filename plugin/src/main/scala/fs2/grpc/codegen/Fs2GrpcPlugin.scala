bd3126240938d99d6babc4bfe2aedf411e247c7f
Alex Henning Johannessen <ahjohannessen@gmail.com>
2023-06-16 13:07:02 +0100

    Revert "Enables code generation for proto in src/test/protobuf"
    This reverts commit ef208f94c66cdb5e16594eddb3f2b3ed93f700bf.

--- plugin/src/main/scala/fs2/grpc/codegen/Fs2GrpcPlugin.scala
+++ plugin/src/main/scala/fs2/grpc/codegen/Fs2GrpcPlugin.scala
@@ -100,10 +100,37 @@ object Fs2GrpcPlugin extends AutoPlugin {
 
   private def codegenScalaBinaryVersion = CrossVersion.binaryScalaVersion(BuildInfo.scalaVersion)
 
-  override def projectSettings: Seq[Def.Setting[_]] = defaultSettings ++ configSettings(Compile) ++ configSettings(Test)
-
-  private def defaultSettings =
+  override def projectSettings: Seq[Def.Setting[_]] =
     List(
+      fs2GrpcServiceSuffix := "Fs2Grpc",
+      fs2GrpcOutputPath := (Compile / sourceManaged).value / "fs2-grpc",
+      scalapbProtobufDirectory := (Compile / sourceManaged).value / "scalapb",
+      scalapbCodeGenerators := {
+        Target(
+          convertOptionsToScalapbGen(scalapbCodeGeneratorOptions.value.toSet),
+          (Compile / sourceManaged).value / "scalapb"
+        ) ::
+          Option(
+            Target(
+              (
+                SandboxedJvmGenerator.forModule(
+                  "scala-fs2-grpc",
+                  Artifact(
+                    BuildInfo.organization,
+                    s"${BuildInfo.codeGeneratorName}_$codegenScalaBinaryVersion",
+                    BuildInfo.version
+                  ),
+                  BuildInfo.codeGeneratorFullName + "$",
+                  Nil
+                ),
+                scalapbCodeGeneratorOptions.value.filterNot(_ == CodeGeneratorOption.Fs2Grpc).map(_.toString) :+
+                  s"serviceSuffix=${fs2GrpcServiceSuffix.value}"
+              ),
+              fs2GrpcOutputPath.value
+            )
+          ).filter(_ => scalapbCodeGeneratorOptions.value.contains(CodeGeneratorOption.Fs2Grpc)).toList
+      },
+      scalapbCodeGeneratorOptions := Seq(CodeGeneratorOption.Grpc, CodeGeneratorOption.Fs2Grpc),
       libraryDependencies ++= List(
         "io.grpc" % "grpc-core" % BuildInfo.grpcVersion,
         "io.grpc" % "grpc-stub" % BuildInfo.grpcVersion,
@@ -111,41 +138,6 @@ object Fs2GrpcPlugin extends AutoPlugin {
         BuildInfo.organization %% BuildInfo.runtimeName % BuildInfo.version,
         "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion,
         "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion
-      )
-    )
-
-  def configSettings(config: Configuration): Seq[Setting[_]] =
-    inConfig(config)(
-      List(
-        fs2GrpcServiceSuffix := "Fs2Grpc",
-        fs2GrpcOutputPath := (config / sourceManaged).value / "fs2-grpc",
-        scalapbProtobufDirectory := (config / sourceManaged).value / "scalapb",
-        scalapbCodeGenerators := {
-          Target(
-            convertOptionsToScalapbGen(scalapbCodeGeneratorOptions.value.toSet),
-            (config / sourceManaged).value / "scalapb"
-          ) ::
-            Option(
-              Target(
-                (
-                  SandboxedJvmGenerator.forModule(
-                    "scala-fs2-grpc",
-                    Artifact(
-                      BuildInfo.organization,
-                      s"${BuildInfo.codeGeneratorName}_$codegenScalaBinaryVersion",
-                      BuildInfo.version
-                    ),
-                    BuildInfo.codeGeneratorFullName + "$",
-                    Nil
-                  ),
-                  scalapbCodeGeneratorOptions.value.filterNot(_ == CodeGeneratorOption.Fs2Grpc).map(_.toString) :+
-                    s"serviceSuffix=${fs2GrpcServiceSuffix.value}"
-                ),
-                fs2GrpcOutputPath.value
-              )
-            ).filter(_ => scalapbCodeGeneratorOptions.value.contains(CodeGeneratorOption.Fs2Grpc)).toList
-        },
-        scalapbCodeGeneratorOptions := Seq(CodeGeneratorOption.Grpc, CodeGeneratorOption.Fs2Grpc)
       )
     )
 }
